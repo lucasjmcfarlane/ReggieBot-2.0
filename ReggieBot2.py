40e60840f2fc9da5f7f1a41d3c476a0d12164c75
Lucas McFarlane <69742154+Yomiko12@users.noreply.github.com>
2023-02-23 19:51:48 -0600

    Upload
    Uploaded all of the code to the GitHub repository

---
+++ ReggieBot2.py
@@ -0,0 +1,35 @@
+import discord
+import reggieEmbeds as e
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+	print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(msg):
+	if msg.author == client.user: return
+	msg.content = msg.content.lower()
+	if msg.content.startswith("r hello"): await e.hello(msg)
+	elif msg.content.startswith("r flipcoin"): await e.flipcoin(msg)
+	elif msg.content.startswith("r insult"): await e.insult(msg)
+	elif msg.content.startswith("r askreggie"): await e.askreggie(msg)
+	# elif msg.content.startswith("r msgfromreggie"): await e.msgfromreggie(msg)
+	# elif msg.content.startswith("r lovecalc"): await e.lovecalc(msg)
+	# elif msg.content.startswith("r owo"): await e.owo(msg)
+	# elif msg.content.startswith("r uwu"): await e.uwu(msg)
+	# elif msg.content.startswith("r ppsize"): await e.ppsize(msg)
+	# elif msg.content.startswith("r rate"): await e.rate(msg)
+	# elif msg.content.startswith("r reggiepic"): await e.reggiepic(msg)
+	# elif msg.content.startswith("r clear"): await e.clear(msg)
+	# elif msg.content.startswith("r pingspam"): await e.pingspam(msg)
+	# elif msg.content.startswith("r reddit"): await e.reddit(msg)
+
+
+
+
+
+client.run('MTA2NzI2MzcxODg1NTAzNjk2OA.GagD6Q.M_hhA8JaMY8CSgB7_1Nar6-JmfOi-aPnAVJXSg')
---
+++ reggieEmbeds.py
@@ -0,0 +1,62 @@
+import discord
+import random
+
+async def hello(msg):
+	embed = discord.Embed(
+		title = "Hi! My name is Reggie!",
+		description = "I am your server's new Discord bot, created by yours truly, Yomiko12#1137",
+		colour = discord.Colour.magenta(),
+	)
+	embed.set_image(url="https://i.pinimg.com/originals/aa/01/90/aa019023148ed37537f9d22fcbe53041.gif")
+	await msg.channel.send(embed = embed)
+
+async def flipcoin(msg):
+	i = random.randint(1,2)
+	if i==1: headsTails = "Heads!"
+	else: headsTails = "Tails!"
+	embed = discord.Embed(
+		title = headsTails,
+		colour = discord.Colour.magenta()
+	)
+	embed.set_image(url="https://media.giphy.com/media/gVROkjyShPnos/giphy.gif")
+	await msg.channel.send(embed = embed)
+
+async def insult(msg):
+	insults = [
+	"You're dumb ",
+	"You're an idiot, ",
+	"Nobody asked, ",
+	"Not gonna lie, You kinda suck ",
+	  "Moron "
+	]
+	name = msg.content[9:]
+	if name=="":
+		message = insults[random.randint(0,len(insults)-1)] + msg.author.name
+	else:
+		message = insults[random.randint(0,len(insults)-1)] + name
+	embed = discord.Embed(
+		title = message,
+		colour = discord.Colour.magenta()
+	)
+	await msg.channel.send(embed = embed)
+
+async def askreggie(msg):
+	responses = [
+		"**YES!!**",
+		"**OF COURSE!!**",
+		"**Yea, probably..**",
+		"**idk..**",
+		"**Maybe..**",
+		"**It's possible..**",
+		"**What kind of question is this?? There must be something seriously wrong with you.**",
+		"**Bruh...**",
+		"**I'm Reggie!**",
+		"**NO!!**",
+		"**Bruh, there is no way..**",
+		"**Nahhh..**"
+	]
+	embed = discord.Embed(
+		title = responses[random.randint(0,len(responses)-1)],
+		colour = discord.Colour.magenta()
+	)
+	await msg.channel.send(embed = embed)
