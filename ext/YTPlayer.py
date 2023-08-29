991676e2686f52afd1d16732b8a95df9d030c265
Lucas McFarlane <126113690+lucasjmcfarlane@users.noreply.github.com>
2023-08-28 23:08:42 -0500

    Add YTPlayer

---
+++ ext/YTPlayer.py
@@ -0,0 +1,116 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+import yt_dlp
+
+voice_clients = {}
+queues = {}
+ctxs = {}
+
+opts = {
+    'format': 'bestaudio/best',
+    'extractaudio': True,
+    'audioformat': 'mp3',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'ytsearch',
+    'source_address': '0.0.0.0',
+}
+ytdl = yt_dlp.YoutubeDL(opts)
+
+ffmpeg_options = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn',
+}
+
+@tasks.loop(seconds=5)
+async def coroutine():
+    for key in voice_clients.keys():
+        if not voice_clients[key].is_playing():
+            await skip(ctxs[key])
+
+@commands.command()
+async def play(ctx, url=""):
+    try:
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+        
+        try: queue = queues[ctx.message.guild.id]
+        except: queues[ctx.message.guild.id] = []
+        queues[ctx.message.guild.id].append(data['url'])
+
+        ctxs[ctx.message.guild.id] = ctx
+
+        try:
+            if voice_clients[ctx.message.guild.id].is_playing():
+                print("already playing, do not start playback")
+        except:
+            await skip(ctx)
+
+        coroutine.start()
+        await ctx.message.add_reaction("👍")
+
+    except Exception as e:
+        print(e)
+        await ctx.message.add_reaction("⚠️")
+
+@commands.command()
+async def skip(ctx):
+    try:
+        voice_client = await ctx.message.author.voice.channel.connect()
+        voice_clients[ctx.message.guild.id] = voice_client
+    except:
+        print("a voice client has already been created!")
+
+    try:
+        player = discord.FFmpegPCMAudio(queues[ctx.message.guild.id].pop(0), **ffmpeg_options)
+        voice_clients[ctx.message.guild.id].stop()
+        voice_clients[ctx.message.guild.id].play(player)
+        await ctx.message.add_reaction("👍")
+    except Exception as e:
+        print(e)
+        await ctx.message.add_reaction("⚠️")
+
+@commands.command()
+async def pause(ctx):
+    try:
+        voice_clients[ctx.message.guild.id].pause()
+        await ctx.message.add_reaction("👍")
+    except Exception as e:
+        print(e)
+        await ctx.message.add_reaction("⚠️")
+
+@commands.command()
+async def resume(ctx):
+    try:
+        voice_clients[ctx.message.guild.id].resume()
+        await ctx.message.add_reaction("👍")
+    except Exception as e:
+        print(e)
+        await ctx.message.add_reaction("⚠️")
+
+@commands.command()
+async def stop(ctx):
+    try:
+        voice_clients[ctx.message.guild.id].stop()
+        await voice_clients[ctx.message.guild.id].disconnect()
+        del voice_clients[ctx.message.guild.id]
+        del queues[ctx.message.guild.id]
+        del ctxs[ctx.message.guild.id]
+        await ctx.message.add_reaction("👍")
+    except Exception as e:
+        print(e)
+        await ctx.message.add_reaction("⚠️")
+
+async def setup(bot):
+    bot.add_command(play)
+    bot.add_command(skip)
+    bot.add_command(pause)
+    bot.add_command(resume)
+    bot.add_command(stop)
--- ext/help.py
+++ ext/help.py
@@ -9,10 +9,20 @@ async def help(ctx):
 		colour = discord.Colour.pink(),
 	)
 
+	embed.add_field(name="AskReggie", value="Ask Reggie a question.", inline=False)
 	embed.add_field(name="Flipcoin", value="Flips a coin.", inline=False)
+	embed.add_field(name="GitHub", value="Sends link to GitHub page.", inline=False)
 	embed.add_field(name="Hello", value="Sends a simple greeting message.", inline=False)
 	embed.add_field(name="Insult", value="Insults the user or the person specified.", inline=False)
-	embed.add_field(name="Msgfromreggue", value="Sends a DM to the person specified.", inline=False)
+	embed.add_field(name="LoveCalc", value="Calculate compatability of two people.", inline=False)
+	embed.add_field(name="Msgfromreggie", value="Sends a DM to the person specified.", inline=False)
+	embed.add_field(name="WelcomeSpeech", value="Sends a welcome message.", inline=False)
+
+	embed.add_field(name="Play", value="Add the given YouTube URL to the play queue", inline=False)
+	embed.add_field(name="Pause", value="Pause the currently playing YouTube video", inline=False)
+	embed.add_field(name="Resume", value="Resume the currently playing YouTube video", inline=False)
+	embed.add_field(name="Skip", value="Skips to the next YouTube video in the queue if there is one", inline=False)
+	embed.add_field(name="Stop", value="Stop audio playback, delete the queue, and disconnect the bot", inline=False)
 
 	await ctx.channel.send(embed = embed)
 
